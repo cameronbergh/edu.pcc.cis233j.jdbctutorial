57a5c05fc7480ac5273c87ffad1c428e6c1d69a0
cameron.bergh <cameron.bergh@outlook.com>
2017-05-01 18:55:46 -0700

    added a bunch of code

---
+++ src/edu/pcc/cis233j/jdbctutorial/FiredupDB.java
@@ -0,0 +1,28 @@
+package edu.pcc.cis233j.jdbctutorial;
+import java.sql.*;
+
+
+/**
+ * Created by SYTC311u27 on 5/1/2017.
+ */
+public class FiredupDB {
+
+    private static final String FIREDUP_URL = "jdbc:jtds:sqlserver://cisdbss.pcc.edu/FiredUp";
+    private static final String USERNAME = "275student";
+    private static final String PASSWORD = "275student";
+
+    private static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(FIREDUP_URL, USERNAME, PASSWORD);
+    }
+
+    public void readCustomers() {
+        try (
+                Connection conn = getConnection();
+                // additional resources will go here
+        ) {
+            System.out.println("Read customers...");
+            // code utilizing resources will go here
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+}
--- src/edu/pcc/cis233j/jdbctutorial/Main.java
+++ src/edu/pcc/cis233j/jdbctutorial/Main.java
@@ -2,9 +2,13 @@ package edu.pcc.cis233j.jdbctutorial;
 
 import static java.lang.System.*;
 
+
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("FiredUp Customers:");
+        FiredupDB firedUp = new FiredupDB();
+        firedUp.readCustomers();
     }
 }
