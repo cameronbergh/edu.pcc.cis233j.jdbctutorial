83c0e2625b8ba3acc4fae1d4a0723aa107e5d327
cameron <cameron.bergh@outlook.com>
2017-06-15 23:50:16 -0700

    Removed unneccesary code.

--- src/edu/pcc/cis233j/jdbctutorial/Main.java
+++ src/edu/pcc/cis233j/jdbctutorial/Main.java
@@ -8,20 +8,9 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("FiredUp Customers:");
-        FiredUpDB firedUp = new FiredUpDB();
-        ArrayList<Customer> customers = firedUp.readCustomers();
-        for (Customer cust : customers) {
+        System.out.println("Git hub Test Project");
 
-            System.out.println("ID: " + cust.getId() + //print customer info
-                    ", Name: " + cust.getName() +
-                    ", City: " + cust.getCity() +
-                    ", State: " + cust.getState());
-
-            ArrayList<String> emailAddys = cust.getEmailAddress(); //get addresses
-            for (String addy : emailAddys) { // print all addresses
-                System.out.println("(" + addy + ")");
-            }
         }
     }
-}
+
+
